1c88869ee4058151fd6235a90b40c2a6ba339ba2
manunasci <nascimentomanuella73@gmail.com>
2021-09-09 16:48:02 -0300

    Adiciona o programa de string

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá meu nome é Cebolinha!");
+            Console.WriteLine("Digite seu texto pala eu lel:");
+
+            string textoDigitado = Console.ReadLine();
+            Console.WriteLine(textoDigitado.Replace("R","L").Replace("r","l"));
+        }
+    }
+}
